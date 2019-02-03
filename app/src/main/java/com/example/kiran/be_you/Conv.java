872434d4233f29c7361f33bbf0dfb50b954aaf51
kirantrathod <kirantrathod.kr@gmail.com>
2019-02-03 20:00:03 +0530

    Revert "Update Firebase Dependancies to current versions and some bug fixes"
    This reverts commit f4e5436

---
+++ app/src/main/java/com/example/kiran/be_you/Conv.java
@@ -0,0 +1,31 @@
+package com.example.kiran.be_you;
+
+public class Conv {
+
+    public boolean seen;
+    public long timestamp;
+
+    public Conv(){
+
+    }
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Conv(boolean seen, long timestamp) {
+        this.seen = seen;
+        this.timestamp = timestamp;
+    }
+}
